e083f2e1da916512e6921ee39385335e4b0d1078
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-26 00:47:19 +0300

    1-task-html

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@
 // import { Profiler } from "./components/Profile";
 // import { TransactionHistory } from "./components/TransactionHistory";
 
-import Profiler from "./components/Profile/Profile";
+import userData from "./components/data/userDate.json";
+
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
     <>
       {/* <Friendlist /> */}
-      <Profiler />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
       {/* <TransactionHistory /> */}
     </>
   );
